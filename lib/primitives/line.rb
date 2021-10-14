e3590c5da100e4d1881f844fe54bec76ce7474e4
Sakorni <ssakorni@gmail.com>
2021-10-14 17:22:10 +0300

    Wrote an empty Line interface

--- lib/primitives/line.rb
+++ lib/primitives/line.rb
@@ -1,3 +1,16 @@
-
 class Line
+  :a
+  :b
+  :c
+
+  # line_equitation = Ax + By + C
+  def initialize(a, b, c)
+    fail "Not implemented"
+  end
+
+  #@return distance between point and current line
+  def distance(point)
+    fail "Not implemented"
+  end
+
 end
