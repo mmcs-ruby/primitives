e93e284d42759b7be98a2efaa4db96245c30202f
Sakorni <ssakorni@gmail.com>
2021-10-17 20:41:49 +0300

    tests added

--- test/point_test.rb
+++ test/point_test.rb
@@ -1,30 +1,8 @@
 
 require "test_helper"
 
-class ElementaryTests < Minitest::Test
+class ElementaryTests < Minitest::Unit::TestCase
   include ::Primitives::Elementary
-
-  @@delta = 1e-5
-
-  def setup
-    @point = Point.new(2.3, 5.1)
-  end
-
-  def test_move_method_correctly_moved_the_point
-    point = Point.new(1,2)
-    point2 = point.move(1, 3)
-    assert_equal 2, point2.x
-    assert_equal 5, point2.y
-    assert_equal 1, point.x
-    assert_equal  1, 2
-  end
-
-  def test_dangerous_move_method_correctly_moved_the_point
-    point = Point.new(1,2)
-    point.move!(1, 3)
-    assert_equal 2, point.x
-    assert_equal 5, point.y
-  end
 
   def testing_initializer
     point = Point.new(1,2)
@@ -36,5 +14,21 @@ class ElementaryTests < Minitest::Test
     assert_raises Point.new(1.2, "3")
   end
 
+
+  def test_move
+    point = Point.new(1,2)
+    point2 = point.move(1, 3)
+    assert_equal 2, point2.x
+    assert_equal 5, point2.y
+    assert_equal 1, point.x
+    assert_equal  1, 2
+  end
+
+  def test_move!
+    point = Point.new(1,2)
+    point.move!(1, 3)
+    assert_equal 2, point.x
+    assert_equal 5, point.y
+  end
 
 end
