4be71ed82067b93a39ae6adf9103b46caa74bb1e
Mikhail Eremeev <meremeev@sfedu.ru>
2021-10-18 23:12:29 +0300

    fix: remove import errors

--- lib/primitives.rb
+++ lib/primitives.rb
@@ -5,8 +5,17 @@ require_relative "primitives/version"
 module Primitives
   class Error < StandardError; end
 
-  require_relative 'primitives/elementary'
-  require_relative 'primitives/shape'
+  require_relative 'primitives/elementary/affine'
+  require_relative 'primitives/elementary/line_segment'
+  require_relative 'primitives/elementary/metric'
+  require_relative 'primitives/elementary/point'
+  require_relative 'primitives/elementary/straight_line'
+
+  require_relative 'primitives/shape/circle'
+  require_relative 'primitives/shape/polygon'
+  require_relative 'primitives/shape/rectangle'
+  require_relative 'primitives/shape/trianlge'
+
   require_relative 'primitives/boolean'
   require_relative 'primitives/layout'
   require_relative 'primitives/plotter'
