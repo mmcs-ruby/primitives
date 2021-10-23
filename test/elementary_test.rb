823a290496b589e011c8643fd5fbeff21b767043
Mikhail Eremeev <meremeev@sfedu.ru>
2021-10-23 12:52:07 +0300

    refactor(tests): set meaningfully testmethod names

--- test/elementary_test.rb
+++ test/elementary_test.rb
@@ -9,6 +9,17 @@ class ElementaryTest < Minitest::Test
     @point = Elementary::Point.new(2.3, 5.1)
   end
 
+  def test_point_attributes_set_correctly_on_instance_create
+    assert_in_delta 2.3, @point.x, @@delta
+    assert_in_delta 5.1, @point.y, @@delta
+  end
+
+  def test_point_initializer_rises_type_error_if_arguments_dont_satisfy_the_requirement_type
+    assert_raises TypeError do
+      Elementary::Point.new(1.2, "3")
+    end
+  end
+
   def test_move_method_correctly_moved_the_point
     p = @point.move 5, 5
 
@@ -22,5 +33,13 @@ class ElementaryTest < Minitest::Test
 
     assert_in_delta 7.3, p.x, @@delta
     assert_in_delta 10.1, p.y, @@delta
+  end
+
+  def test_move_method_doesnt_affect_point_instance_on_which_it_was_applied
+    original = @point
+    _ = original.move 777, 777
+
+    assert_in_delta 2.3, original.x, @@delta
+    assert_in_delta 5.1, original.y, @@delta
   end
 end
