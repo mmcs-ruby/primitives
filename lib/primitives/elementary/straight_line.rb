3cac79ff9b19b05c0229a22b97ddf524ae9d2ba1
Moberd <ocheretov.ilya@yandex.ru>
2021-11-04 13:10:15 +0300

    created test, some rubycop changes

--- lib/primitives/elementary/straight_line.rb
+++ lib/primitives/elementary/straight_line.rb
@@ -1,13 +1,22 @@
+# frozen_string_literal: true
+
 module Primitives
   module Elementary
     # Ax + By + C = 0
     class StraightLine
-      attr_reader :a
-      attr_reader :b
-      attr_reader :c
+      attr_reader :a, :b, :c
 
       def initialize(a, b, c)
-        # Your code goes gere...
+        check_argument(a)
+        check_argument(b)
+        check_argument(c)
+        @a = a
+        @b = b
+        @c = c
+      end
+
+      def check_argument(arg)
+        raise TypeError, "Invalid type of argument (Must be Numeric)" unless arg.is_a?(Numeric)
       end
     end
   end
---
+++ test/straightline_test_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class StraightLine_testTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end
